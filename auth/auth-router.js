e4ee366e31839e6d9735f1815f257a7ec1a9e2c8
Trey McGarity <trey.mcgarity99@gmail.com>
2020-03-19 18:36:20 -0500

    not mvp because of get '/users'

---
+++ auth/auth-router.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const Users = require("../users/users-model")
+
+const router = express.Router()
+
+router.post("/register", async (req, res, next) => {
+	try {
+		const { username } = req.body
+		const user = await Users.findBy({ username }).first()
+
+		if (user) {
+			return res.status(409).json({
+				message: "Username is already taken",
+			})
+		}
+
+		res.status(201).json(await Users.add(req.body))
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.post("/login", async (req, res, next) => {
+	const authError = {
+		message: "Invalid Credentials",
+	}
+
+	try {
+		const { username, password } = req.body
+
+		const user = await Users.findBy({ username }).first()
+		if (!user) {
+			return res.status(401).json(authError)
+		}
+
+		const passwordValid = await bcrypt.compare(password, user.password)
+		if (!passwordValid) {
+			return res.status(401).json(authError)
+		}
+
+		const payload = {
+			userId: user.id,
+			userRole: "admin", // normally from a database
+		}
+		const token = jwt.sign(payload, process.env.JWT_SECRET)
+
+		res.cookie("token", token)
+		res.json({
+			message: `Welcome ${user.username}!`,
+		})
+	} catch(err) {
+		next(err)
+	}
+})
+
+module.exports = router
---
+++ users/users-router.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const Users = require("./users-model")
+const restrict = require("../middleware/restrict")
+const restrictRole = require("../middleware/restrict-role")
+
+const router = express.Router()
+
+router.get("/", restrict(), restrictRole("admin"), async (req, res, next) => {
+	try {
+		res.json(await Users.find())
+	} catch(err) {
+		next(err)
+	}
+})
+
+module.exports = router
